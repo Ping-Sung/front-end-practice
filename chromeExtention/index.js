12ac922be90d278057d05f820484c107e3777ef0
Ping-Sung <76198720+Ping-Sung@users.noreply.github.com>
2023-03-12 12:13:14 +0800

    Chrome extension
    New small project.

--- chromeExtention/index.js
+++ chromeExtention/index.js
@@ -1,28 +1,51 @@
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
-let myLeads = []
-
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const inputBtn = document.getElementById('input-btn')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
-inputBtn.addEventListener('click', function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    console.log(ulEl)
-    renderLeads()
+let myLeads = []
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+    // console.log(myLeads)
+}
+
+tabBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        saveleads(tabs[0].url)
+    })
+    
 })
 
-function renderLeads() {
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener('click', function(){
+    saveleads(inputEl.value)
+    inputEl.value = ""
+})
+
+function saveleads(site) {
+    myLeads.push(site)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+}
+
+function render(leads) {
     listItem = ""
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
         // li = '<li><a href="' + myLeads[i] + '" target="_blank">' + myLeads[i] + '<a></li>'
 
         li = `
             <li>
-                <a href="${myLeads[i]}" target="_blank">
-                    ${myLeads[i]}
+                <a href="${leads[i]}" target="_blank">
+                    ${leads[i]}
                 <a>
             </li>
         `
